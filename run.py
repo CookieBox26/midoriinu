2a2a7b2f746f301c280b58228ede6cedf3b47204
CookieBox26 <cookie-box@cookie-box.info>
2025-08-07 03:15:39 +0000

    Fix

--- run.py
+++ run.py
@@ -1,5 +1,50 @@
-from vllm import LLM
+import json
+from openai_harmony import (
+    HarmonyEncodingName,
+    load_harmony_encoding,
+    Conversation,
+    Message,
+    Role,
+    SystemContent,
+    DeveloperContent,
+)
+from vllm import LLM, SamplingParams
 
-llm = LLM('openai/gpt-oss-120b', tensor_parallel_size=4)
-resp = llm.generate('リーマン予想について教えてください。')
-print(resp[0].outputs[0].text)
+
+def main():
+    encoding = load_harmony_encoding(HarmonyEncodingName.HARMONY_GPT_OSS)
+    convo = Conversation.from_messages([
+        Message.from_role_and_content(Role.SYSTEM, SystemContent.new()),
+        Message.from_role_and_content(
+            Role.DEVELOPER,
+            DeveloperContent.new().with_instructions("Always respond in riddles"),
+        ),
+        Message.from_role_and_content(Role.USER, "What is the weather like in SF?"),
+    ])
+    prefill_ids = encoding.render_conversation_for_completion(convo, Role.ASSISTANT)
+    stop_token_ids = encoding.stop_tokens_for_assistant_actions()
+
+    llm = LLM(
+        model="openai/gpt-oss-120b",
+        trust_remote_code=True,
+    )
+    sampling = SamplingParams(
+        max_tokens=128,
+        temperature=1,
+        stop_token_ids=stop_token_ids,
+    )
+    outputs = llm.generate(
+        prompt_token_ids=[prefill_ids],   # batch of size 1
+        sampling_params=sampling,
+    )
+
+    gen = outputs[0].outputs[0]
+    text = gen.text
+    output_tokens = gen.token_ids
+    entries = encoding.parse_messages_from_completion_tokens(output_tokens, Role.ASSISTANT)
+    for message in entries:
+        print(f'{json.dumps(message.to_dict())}')
+
+
+if __name__ == '__main__':
+    main()
